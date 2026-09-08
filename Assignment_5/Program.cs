21e91670f2f953d7eddb8a42a008e9fc6a559e80
Nada Ezz <nadaezzeldein6@gmail.com>
2026-09-09 02:47:44 +0300

    Add project files.

---
+++ Assignment_5/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignment_5
+{
+    internal class Program
+    {
+        //class Book
+        //{
+        //    private string password = "secret";
+        //}
+        static void Main(string[] args)
+        {
+            //#region Question 1
+            //Book myBook = new Book();
+            //Console.WriteLine(myBook.password); // Error
+            //#endregion
+        }
+    }
+}
